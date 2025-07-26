4f3106c372c382daea7a4d38465bb95f2723fed4
zakyabdurrahman <meansproducer02@gmail.com>
2025-07-27 06:28:05 +0700

    added services endpoint

--- controllers/informationController.js
+++ controllers/informationController.js
@@ -2,28 +2,43 @@ const conn = require('../database/connection.js');
 const InfoQuery = require('../database/infoQuery.js');
 
 class InformationController {
-
-    static async getBanners(req, res, next) {
-        try {
-            
-            let rawData = await conn.query(InfoQuery.bannerQuery);
-            let {rows} = rawData
-            let data = rows.map((e) => {
-                delete e['banner_id'];
-                return e;
-            });
-            res.status(200).json({
-                status: 0,
-                message: 'Sukses',
-                data: data
-            })
-        } catch(error) {
-            console.log(error)
-            next(error)
-        }
+  static async getBanners(req, res, next) {
+    try {
+      let rawData = await conn.query(InfoQuery.bannerQuery);
+      let { rows } = rawData;
+      let data = rows.map((e) => {
+        delete e["banner_id"];
+        return e;
+      });
+      res.status(200).json({
+        status: 0,
+        message: "Sukses",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
+  }
 
-    
+  static async getServices(req, res, next) {
+    try {
+      let rawData = await conn.query(InfoQuery.serviceQuery);
+      let { rows } = rawData;
+      let data = rows.map((e) => {
+        delete e["service_id"];
+        return e;
+      });
+      res.status(200).json({
+        status: 0,
+        message: "Sukses",
+        data: data,
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
 }
 
 module.exports = InformationController;
--- routers/informationRouter.js
+++ routers/informationRouter.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const InformationController = require('../controllers/informationController');
+const authentication = require('../middlewares/authentication');
 
 router.get('/banner', InformationController.getBanners);
-
+router.get('/services', authentication, InformationController.getServices);
 
 module.exports = router;
